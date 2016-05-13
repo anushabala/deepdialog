939462e12fb3e2f318a4b7fd527760031097920a
Anusha Balakrishnan <anusha.bala28@gmail.com>
2016-05-13 05:01:15 -0700

    updated html report script for dating scenario

--- chat/utils/create_html_template.py
+++ chat/utils/create_html_template.py
@@ -5,7 +5,7 @@ from argparse import ArgumentParser
 from transcript_utils import parse_transcript_dating, parse_transcript, load_scenarios, NO_OUTCOME
 
 
-def create_html_lines(chat_name, transcript, html_lines):
+def create_html_lines(chat_name, transcript, complete_html, incomplete_html):
     selections = {0:None, 1:None}
     chat_html= ['<h4>%s</h4>' % chat_name, '<table border=\"1\", style=\"border-collapse: collapse\">', '<tr>', '<td width=\"50%%\">']
     ended = 0
@@ -45,14 +45,18 @@ def create_html_lines(chat_name, transcript, html_lines):
         chat_html.insert(0, '<div style=\"color:#FF0000\">')
     chat_html.append('</div>')
 
-    html_lines.extend(chat_html)
+    if completed:
+        complete_html.extend(chat_html)
+    else:
+        incomplete_html.extend(chat_html)
 
     return completed
 
 
 def aggregate_chats(scenario_type='friends'):
     html = ['<!DOCTYPE html>','<html>']
-    chats = []
+    complete_chats = []
+    incomplete_chats = []
     total = 0
     num_completed = 0
     for f in os.listdir(args.dir):
@@ -61,12 +65,13 @@ def aggregate_chats(scenario_type='friends'):
             transcript = parse_transcript(os.path.join(args.dir, f))
         else:
             transcript = parse_transcript_dating(os.path.join(args.dir, f), scenarios)
-        completed = create_html_lines(f, transcript, chats)
+        completed = create_html_lines(f, transcript, complete_chats, incomplete_chats)
         if completed:
             num_completed += 1
         total += 1
     html.extend(['<h3>Total number of chats: %d</h3>' % total, '<h3>Number of chats completed: %d</h3>' % num_completed])
-    html.extend(chats)
+    html.extend(complete_chats)
+    html.extend(incomplete_chats)
     html.append('</html>')
     return html
 
