859af40b880e75a644cba2f0fbe02495d3a12154
Anusha Balakrishnan <anusha@jagupard11.stanford.edu>
2016-05-30 18:46:47 -0700

    minor fix to candidate sampling code

--- chat/nn/sample_candidates.py
+++ chat/nn/sample_candidates.py
@@ -1,9 +1,10 @@
 __author__ = 'anushabala'
 import numpy as np
 
-START_TOKEN = "START"
+START_TOKEN = "_start_dialogue_"
 SELECT_TOKEN = "_select_name_"
-SAY_TOKEN = "SAY"
+SAY_TOKEN = "_say_"
+PASS_TOKEN = "_action_pass_" 
 
 
 def sample_state(example):
@@ -27,15 +28,24 @@ def sample_logical_form(message):
 
 
 def create_sequences_from_dialogue(dialogue, this_agent):
-    print dialogue
+    #print dialogue
+    #print type(this_agent)
     first_agent = dialogue[0][0]
+    #print type(first_agent)
     other_agent = 1 - this_agent
+    #print first_agent
+    #print this_agent
+    #print other_agent
+    #print first_agent == this_agent
     if first_agent != this_agent:
         dialogue[0][1].insert(0, START_TOKEN)
+        current_agent = first_agent
+
     else:
         dialogue.insert(0, (other_agent, [START_TOKEN]))
+        current_agent = other_agent
 
-    current_agent = first_agent
+    #print dialogue
     x = []
     y = []
     current_seqs = []
@@ -45,12 +55,17 @@ def create_sequences_from_dialogue(dialogue, this_agent):
 
         seq = " ".join(tokens)
         if current_agent != agent:
-            agent_list = y if agent == this_agent else x
+            agent_list = y if current_agent == this_agent else x
             agent_list.append(" ".join(current_seqs))
             current_agent = agent
             current_seqs = []
         current_seqs.append(seq)
 
+    agent_list = y if current_agent == this_agent else x
+    agent_list.append(" ".join(current_seqs))
+
+    if current_agent == other_agent and len(x) == len(y)+1:
+        y.append(PASS_TOKEN)
     return x, y
 
 
